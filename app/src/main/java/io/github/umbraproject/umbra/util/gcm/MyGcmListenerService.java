f2b0b810488aba1dfe9151b3fd369ccfd11c911a
matthewcmckenna <matt.c.mckenna@gmail.com>
2016-05-03 17:55:20 -0400

    initial implementation of GcmListenerService

--- app/src/main/java/io/github/umbraproject/umbra/util/gcm/MyGcmListenerService.java
+++ app/src/main/java/io/github/umbraproject/umbra/util/gcm/MyGcmListenerService.java
@@ -2,16 +2,20 @@ package io.github.umbraproject.umbra.util.gcm;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+
+import com.google.android.gms.gcm.GcmListenerService;
 
 /**
  * Created by matt on 4/26/16.
  */
-public class MyGcmListenerService extends Service{
-    @Nullable
+public class MyGcmListenerService extends GcmListenerService {
+    public static final String TAG = MyGcmListenerService.class.getSimpleName();
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onMessageReceived(String from, Bundle data) {
+        super.onMessageReceived(from, data);
     }
 }
